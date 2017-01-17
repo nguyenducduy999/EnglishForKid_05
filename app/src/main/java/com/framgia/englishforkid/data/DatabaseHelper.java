670defcc9fd600ebf6d05ce8a2c210bcfe8a640a
DuyNguyenduc <nguyen.duc.duy@framgia.com>
2017-01-17 17:33:40 +0700

    local data

---
+++ app/src/main/java/com/framgia/englishforkid/data/DatabaseHelper.java
@@ -0,0 +1,40 @@
+package com.framgia.englishforkid.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.framgia.englishforkid.data.EnglishForKidProvider.DATABASE_NAME;
+import static com.framgia.englishforkid.data.EnglishForKidProvider.DATABASE_VERSION;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String FIELD_ID = "_id";
+    public static final String FIELD_TITLE = "title";
+    public static final String FIELD_URL_VIDEO = "url_video";
+    public static final String FIELD_URL_IMG = "url_image";
+    public static final String FIELD_TYPE = "type";
+    public static final int TYPE_SONG = 0;
+    public static final int TYPE_STORY = 1;
+    public static final String TABLE_NAME = "english_for_kid";
+    public static final String COMMAND_CREATE_DB = " CREATE TABLE " + TABLE_NAME
+            + " (FIELD_ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + FIELD_TITLE + " TEXT NOT NULL, " + FIELD_URL_VIDEO + " TEXT NOT NULL, "
+            + FIELD_URL_IMG + " TEXT NOT NULL, "
+            + FIELD_TYPE + " INTERGER NOT NULL);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(COMMAND_CREATE_DB);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
+
---
+++ app/src/main/java/com/framgia/englishforkid/data/EnglishForKidProvider.java
@@ -0,0 +1,147 @@
+package com.framgia.englishforkid.data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import static com.framgia.englishforkid.data.DatabaseHelper.FIELD_ID;
+import static com.framgia.englishforkid.data.DatabaseHelper.TABLE_NAME;
+
+public class EnglishForKidProvider extends ContentProvider {
+    public static final String PROVIDER_NAME = "com.framgia.EnglishForKidProvider";
+    public static final String URL_DATA_BASE = "content://" + PROVIDER_NAME + "/datas";
+    public static final Uri CONTENT_URI = Uri.parse(URL_DATA_BASE);
+
+    public static final int URI_CODE = 1;
+    public static final int URI_CODE_ID = 2;
+
+    public static final UriMatcher sUriMatcher;
+    private SQLiteDatabase mDatabase;
+    static final String DATABASE_NAME = "English_For_Kid";
+    static final int DATABASE_VERSION = 1;
+
+    static {
+        sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        sUriMatcher.addURI(PROVIDER_NAME, "datas", URI_CODE);
+        sUriMatcher.addURI(PROVIDER_NAME, "datas/#", URI_CODE);
+    }
+
+    public EnglishForKidProvider() {
+
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int count = 0;
+        switch (sUriMatcher.match(uri)) {
+            case URI_CODE:
+                count = mDatabase.delete(TABLE_NAME, selection, selectionArgs);
+                break;
+            case URI_CODE_ID:
+                String id = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    count = mDatabase.delete(
+                            TABLE_NAME,
+                            FIELD_ID + "=" + id,
+                            null);
+                } else {
+                    count = mDatabase.delete(
+                            TABLE_NAME,
+                            FIELD_ID + "=" + id
+                                    + " and " + selection,
+                            selectionArgs);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        long rowID = 0;
+        switch (sUriMatcher.match(uri)) {
+            case URI_CODE:
+                rowID = mDatabase.insert(TABLE_NAME, null, values);
+                if (rowID <= 0) return null;
+                Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+                getContext().getContentResolver().notifyChange(_uri, null);
+                return _uri;
+            default:
+                throw new SQLException("Failed to add a record into " + uri);
+        }
+
+    }
+
+    @Override
+    public boolean onCreate() {
+        DatabaseHelper dbHelper = new DatabaseHelper(getContext());
+        mDatabase = dbHelper.getWritableDatabase();
+        if (mDatabase == null) return false;
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(TABLE_NAME);
+
+        switch (sUriMatcher.match(uri)) {
+            case URI_CODE_ID:
+                qb.appendWhere(FIELD_ID + "=" + uri.getLastPathSegment());
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        Cursor cursor = qb.query(mDatabase, projection, selection, selectionArgs, null,
+                null, sortOrder);
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        int count = 0;
+        switch (sUriMatcher.match(uri)) {
+            case URI_CODE:
+                count = mDatabase.update(TABLE_NAME, values, selection, selectionArgs);
+                break;
+            case URI_CODE_ID:
+                String id = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    count = mDatabase.update(TABLE_NAME,
+                            values, FIELD_ID + "=" + id, null);
+                } else {
+                    count = mDatabase.update(TABLE_NAME,
+                            values, FIELD_ID + "=" + id
+                                    + " and "
+                                    + selection,
+                            selectionArgs);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+}
+
